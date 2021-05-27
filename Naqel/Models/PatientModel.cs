66605bf0112809a5d977ba2d08a384c5a737dec4
perle <perle@DESKTOP-UC5BINN>
2021-05-27 01:02:47 +0100

    __

---
+++ Naqel/Models/PatientModel.cs
@@ -0,0 +1,29 @@
+﻿using BMSBahrain.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BMSBahrain.Models
+{
+    public class PatientModel: BaseViewModel
+    {
+        public string Id { get; set; }
+        public bool IsFirstVisit { get; set; }
+        public string DiseaseName { get; set; }
+        public string DiseaseDescription { get; set; }
+        public DateTime ProblemDay { get; set; }
+        public bool IsTakenMedicine { get; set; }
+        public string Medicine { get; set; }
+        public string MedicineImageUrl { get; set; }
+        public bool HaveAllergy { get; set; }
+        public string AllergyDescription { get; set; }
+        public bool HaveRegularMedicine { get; set; }
+        public DateTime LastVisit { get; set; }
+        public bool IsImproved { get; set; }
+        public double ImprovmentPercentage { get; set; }
+
+        public string LastMedicineImage { get => _LastMedicineImage; set => SetProperty(ref _LastMedicineImage , value); }
+
+        string _LastMedicineImage;    
+    }
+}
